18c625b8fe9e38b19fdfdcf727bc25696d1c1073
Jez Thompson <mailme@jezthompson.co.uk>
2017-07-26 11:50:00 +0100

    Update header logo html.

--- template-parts/header/header-branding.php
+++ template-parts/header/header-branding.php
@@ -10,13 +10,8 @@
 
 ?>
 <div class="site-branding">
-	<?php
-	if ( is_front_page() && is_home() ) : ?>
-		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-	<?php else : ?>
-		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-	<?php endif; ?>
-	
-	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/header-logo.png" width="300" height="168" alt="Site Logo">
+	<a href="<?php echo esc_url( home_url( '/' ) ); ?>">
+		<img class="lazyload" data-srcset="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logos/main-logo.svg" src="<?php echo esc_url( get_template_directory_uri() ); ?>/images/logos/main-logo.svg" alt="<?php bloginfo( 'name' ); ?> Logo" />
+	</a>
 	
 </div><!-- .site-branding -->
